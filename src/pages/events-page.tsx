6df43f91ae8956e84372f7668c0f3c4f552f9897
KyryloPikhno <Mr.Good@i.ua>
2023-05-03 22:15:09 +0300

    fixed routing

--- src/pages/events-page.tsx
+++ src/pages/events-page.tsx
@@ -1,13 +1,13 @@
 import {SideBar} from "@/components/SideBar";
 import {Events} from "@/components/Events";
 
+
 export default function EventsPage() {
+
     return (
         <div>
-            <SideBar/>
-            <div>
-              <Events/>
-            </div>
+        <SideBar/>
+            <Events/>
         </div>
     );
 };
