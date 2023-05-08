31ccf15bb1fdf774fe466ab2d12757b21405369b
KyryloPikhno <Mr.Good@i.ua>
2023-05-08 12:08:57 +0300

    refactoring code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.resolve.alias['graphql'] = 'graphql'
+    return config
+  }
 }
 
 module.exports = nextConfig
