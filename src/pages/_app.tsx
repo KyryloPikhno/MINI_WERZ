14dd61573b370be42755bba7045d7a0b14164193
KyryloPikhno <Mr.Good@i.ua>
2023-05-10 15:14:39 +0300

    added css files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import {MemoizedSideBar} from "@/components/SideBar";
+import {AuthChecker} from "@/components/AuthChecker";
 import {client} from "@/configs/apollo-client";
 import {ApolloProvider} from "@apollo/client";
 import {useMemoOne} from "use-memo-one";
-import type {AppProps} from 'next/app';
+import type {AppProps} from "next/app";
 import {useRouter} from "next/router";
-import '@/styles/globals.css'
-import {AuthChecker} from "@/components/AuthChecker";
+import "@/styles/searchForm.css";
+import "@/styles/pagination.css";
+import "@/styles/eventList.css";
+import "@/styles/sideBar.css";
+import "@/styles/globals.css";
+import "@/styles/login.css";
 
 
 export default function App({Component, pageProps}: AppProps) {
